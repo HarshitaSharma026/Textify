dfde7a43a54f05f9ad08ade64273b8594484bcbe
HarshitaSharma026 <harshita026.sharma@gmail.com>
2024-06-01 23:09:07 +0530

    Bootstrap added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // where there is id = "root", add App componenet there
   <React.StrictMode>
     <App />
   </React.StrictMode>
